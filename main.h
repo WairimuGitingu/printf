c95f264d5e18b06e55b33d2cd5a5e5e2d7241fdf
Mdigo12 <mdigojacob@gmail.com>
2022-07-10 10:53:40 -0700

    main.h, Added specifiers prototypes

--- main.h
+++ main.h
@@ -6,6 +6,10 @@ typedef struct print_all
 	char *c;
 	int (*p)(va_list);
 }print_all_t;
+
 int _printf(const char *format, ...);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_per(int p);
 
 #endif
