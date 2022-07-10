17ac3ded859e58f20090a4d8324b272b018d247f
Mdigo12 <mdigojacob@gmail.com>
2022-07-10 09:28:47 -0700

    0-printf.c skel

--- 0-printf.c
+++ 0-printf.c
@@ -1,11 +1,16 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlid.>
+#include <stdlib.h>
 
 /**
- * _printf - 
- * 
+ * _printf - a function that produces output according to a format
+ * @format: is a character string
+ *
+ * Return: the number of characters printed
+ *	 (excluding the null byte used to end output to strings)
+ */
 int _printf(const char *format, ...)
 {
-	var_list args;
+	va_list args;
+	va_start
 }
