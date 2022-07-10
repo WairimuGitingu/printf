48e5fef74a773ef10ccfc91b11d1fbe88de05c7f
WairimuGitingu <wairimugitungu@gmail.com>
2022-07-10 10:21:31 -0700

    specifiers.c print string

--- specifiers.c
+++ specifiers.c
@@ -16,3 +16,24 @@ int print_c(va_list c)
 	_putchar(ch);
 	return (1);
 }
+
+/**
+ * print_s - prints out string
+ * @s: string to print
+ *
+ * Return: number of characters printed, i
+ */
+int print_s(va_list s)
+{
+	int i = 0;
+	char *st = va_arg(s, char *);
+
+	if (st == NULL)
+		st = "(null)";
+	while (st[i])
+	{
+		_putchar(st[i]);
+		i++;
+	}
+	return(i);
+}
