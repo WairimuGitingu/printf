7546bcc63a422ae50bee271f28580921641a0870
Mdigo12 <mdigojacob@gmail.com>
2022-07-11 02:00:54 -0700

    print_c arg to be cha

--- main.h
+++ main.h
@@ -8,7 +8,7 @@ typedef struct print_all
 }print_all_t;
 
 int _printf(const char *format, ...);
-int print_c(va_list c);
+int print_c(va_list cha);
 int print_s(va_list s);
 int print_per(va_list p);
 
